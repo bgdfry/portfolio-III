9a24a4d4b9962088a493681c707b86c84e451e3a
Ben <bcgodfrey91@gmail.com>
2022-01-25 16:04:42 -0600

    initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '../app.css';
+</script>
+
+<!-- <Header /> -->
+
+<main>
+	<slot />
+</main>
+
+<!-- <footer> </footer> -->
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+</style>
